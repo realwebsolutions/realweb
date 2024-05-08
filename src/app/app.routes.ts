ddd11be022257748585518e3f42357470680afe5
aashinsiby <aashin2000@gmail.com>
2024-05-08 21:12:49 +0530

    13

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { KnowledgeComponent } from './knowledge/knowledge.component';
+import { SpaceComponent } from './space/space.component';
+import { VisionComponent } from './vision/vision.component';
+import { FutureComponent } from './future/future.component';
 
 export const routes: Routes = [
     { path:'', redirectTo: 'home', pathMatch:"full"},
-    { path: 'home', component: HomeComponent},
+    { path: 'home', component: HomeComponent},  
+    { path: 'knowledge', component: KnowledgeComponent}, 
+     { path: 'space', component: SpaceComponent},  
+    { path: 'vision', component: VisionComponent},  
+    { path: 'future', component: FutureComponent},
 ];
